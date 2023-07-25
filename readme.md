225619021dd2c7ef70037e0a2c3975c714ec56f6
Veronica <almeida.vrc@gmail.com>
2023-07-24 21:18:39 -0300

    add readme.md, Mentoria and Curso

---
+++ readme.md
@@ -0,0 +1,17 @@
+<h1> Desafio: Aprenda na Prática Programação Orientada a Objetos</h1>
+
+
+
+
+
+
+
+<h2> Passo-a-Passo</h2>
+
+
+1. Vamos ABSTRAIR o DOMÍNIO Bootcamp e MODELAR seus ATRIBUTOS E MÉTODOS <br><br>
+2. Criaremos as CLASSES: Bootcamp, Cursos, Mentorias e Devs e vamos relaciona-las <br><br>
+3. As CLASSES Curso, Mentoria e Devs também serão MODELADOS, ou seja, criaremos seus ATRIBUTOS E MÉTODOS <br><br> 
+4. Para que o código fique mais legível e de fácil manutenção, iremos utilizar de algumas das ferramentas que o PARADIGMA DE ORIENTAÇÃO A OBJETOS (POO) nos oferece: ABSTRAÇÃO, ENCAPSULAMENTO, HERANÇA E POLIMORFISMO <br><br>
+5. E para representar CLASSES que foram criadas e relacionadas, iremos transforma-las em OBJETOS<br><br>
+
---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Descrição do curso de Java");
+        curso1.setCargaHoraria(8);
+
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de js");
+        curso2.setDescricao("Descrição do curso de js");
+        curso2.setCargaHoraria(4);
+
+
+
+        Mentoria mentoria1 = new Mentoria();
+
+        mentoria1.setTitulo("Mentoria de java");
+        mentoria1.setDescricao("Descrição da mentoria de java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
